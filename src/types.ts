4fdfd96228235a4b2c5c1c74d057307bb8e6615a
MadsSRasmussen <madsrasmussendev@gmail.com>
2025-04-29 21:58:53 +0200

    docs: add docs to proxy server types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,15 @@
+/**
+ * Configuration settings for the proxy handler.
+ */
 export type ProxyHandlerSettings = {
     target: string;
     headers: Record<string, string>;
     changeHost: boolean;
 };
 
+/**
+ * Configuration settings for the proxy server.
+ */
 export type ProxyConfig = ProxyHandlerSettings & {
     port: number;
 };
